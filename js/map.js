0c1c2fc403ab145f4a94dcc3d51560ad276ef6ab
svetlana romanova <svetlanakrbva@rambler.ru>
2017-12-03 23:52:46 +0300

    Удалено многократное клонирование

--- js/map.js
+++ js/map.js
@@ -90,9 +90,11 @@ mapPins.appendChild(fragment);
 
 var map = document.querySelector('.map');
 var mapTemplate = document.querySelector('template').content.querySelector('article');
+var mapTemplateCopy = mapTemplate.cloneNode(true);
+map.appendChild(mapTemplateCopy);
+
 
 var showPopup = function (pinData) {
-  var mapTemplateCopy = mapTemplate.cloneNode(true);
   var typeBuilding = mapTemplateCopy.querySelector('h4');
   var popupFeatures = mapTemplateCopy.querySelector('.popup__features');
   var popupP = mapTemplateCopy.querySelectorAll('p');
@@ -122,12 +124,15 @@ var showPopup = function (pinData) {
       }
     }
   }
-  map.appendChild(mapTemplateCopy);
+  return mapTemplateCopy;
 };
 
 var form = document.querySelector('.notice__form');
 var mainPin = document.querySelector('.map__pin--main');
 var mapPin = document.querySelectorAll('.map__pin');
+var popup = document.querySelector('.popup');
+var article = document.querySelector('article');
+var elPopupClose = popup.querySelector('.popup__close');
 
 mainPin.addEventListener('mouseup', function () {
   map.classList.remove('map--faded');
@@ -136,46 +141,45 @@ mainPin.addEventListener('mouseup', function () {
 
 var onPopupEscPress = function (evt) {
   if (evt.keyCode === ESC_KEYCODE) {
-    map.removeChild(document.querySelector('.popup'));
+    article.classList.add('hidden');
   }
 };
 
-var popupOpen = function (a, b) {
-  showPopup(realAds[+a]);
+var handleSelection = function (a) {
+  showPopup(realAds[+a.id]);
+  article.classList.remove('hidden');
   for (var j = 1; j < mapPin.length; j++) {
     if (mapPin[j].classList.contains('map__pin--active')) {
       mapPin[j].classList.remove('map__pin--active');
     }
   }
-  b.classList.add('map__pin--active');
+  a.classList.add('map__pin--active');
 };
 
 var popupClose = function () {
-  var popup = document.querySelector('.popup');
-  popup.querySelector('.popup__close').addEventListener('click', function () {
-    map.removeChild(popup);
+  elPopupClose.addEventListener('click', function () {
+    article.classList.add('hidden');
   });
-  document.addEventListener('keydown', onPopupEscPress);
 };
-
-for (var i = 1; i < mapPin.length; i++) {
-  mapPin[i].addEventListener('click', function () {
-    popupOpen(this.id, this);
-    popupClose();
-  });
-}
 
 for (var i = 1; i < mapPin.length; i++) {
   mapPin[i].addEventListener('keydown', function (evt) {
     if (evt.keyCode === ENTER_KEYCODE) {
-      popupOpen(this.id, this);
+      handleSelection(this);
     }
-    popupClose();
-    popup.querySelector('.popup__close').addEventListener('keydown', function (evt) {
+    document.addEventListener('keydown', onPopupEscPress);
+    elPopupClose.addEventListener('keydown', function (evt) {
       if (evt.keyCode === ENTER_KEYCODE) {
-        map.removeChild(popup);
+        popupClose();
       }
     });
   });
 }
 
+for (var i = 1; i < mapPin.length; i++) {
+  mapPin[i].addEventListener('click', function () {
+    handleSelection(this);
+    popupClose();
+    document.addEventListener('keydown', onPopupEscPress);
+  });
+}
