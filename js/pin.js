4bf932f512606c4ff43eb2a2103375db18a61207
svetlana romanova <svetlanakrbva@rambler.ru>
2017-12-22 17:29:49 +0300

    Добавлен дебаунс и доделаны фильтры

--- js/pin.js
+++ js/pin.js
@@ -3,14 +3,12 @@
 (function () {
   var ENTER_KEYCODE = 13;
   var ESC_KEYCODE = 27;
+
   var mapPins = document.querySelector('.map__pins');
   var filterHousingType = document.querySelector('#housing-type');
   var filterPrice = document.querySelector('#housing-price');
   var filterRooms = document.querySelector('#housing-rooms');
   var filterGuests = document.querySelector('#housing-guests');
-  var filterFeatures = document.querySelector('#housing-features');
-  var filterInputFeatures = filterFeatures.querySelectorAll('input');
-  var filterInputFeature = filterFeatures.querySelector('input');
   var filterWifi = document.querySelector('#filter-wifi');
   var filterDishwasher = document.querySelector('#filter-dishwasher');
   var filterParking = document.querySelector('#filter-parking');
@@ -23,18 +21,18 @@
     window.data = authors;
     window.pinsArray = [];
     var fragment = document.createDocumentFragment();
-    for (var j = 0; j < 8; j++) {
+    for (var i = 0; i < 8; i++) {
 
       var butt = document.createElement('button');
       butt.className = 'map__pin hidden';
-      butt.style.cssText = 'left:' + authors[j].location.x + 'px; top:  ' + authors[j].location.y + 'px';
+      butt.style.cssText = 'left:' + authors[i].location.x + 'px; top:  ' + authors[i].location.y + 'px';
       butt.style.tabIndex = '0';
-      butt.id = j;
+      butt.id = i;
 
       fragment.appendChild(butt);
 
       var img = document.createElement('img');
-      img.src = authors[j].author.avatar;
+      img.src = authors[i].author.avatar;
       img.width = 40;
       img.height = 40;
       img.draggable = false;
@@ -82,14 +80,14 @@
         }
       });
 
-      for (var i = 1; i < elMapPins.length; i++) {
-        elMapPins[i].addEventListener('click', function (evt) {
+      for (var k = 1; k < elMapPins.length; k++) {
+        elMapPins[k].addEventListener('click', function () {
           handleSelection(this);
         });
       }
 
-      for (var i = 1; i < elMapPins.length; i++) {
-        elMapPins[i].addEventListener('keydown', function (evt) {
+      for (var l = 1; l < elMapPins.length; l++) {
+        elMapPins[l].addEventListener('keydown', function (evt) {
           if (evt.keyCode === ENTER_KEYCODE) {
             handleSelection(this);
           }
@@ -109,59 +107,74 @@
 
   window.backend.load(successHandler, errorHandler);
 
+  var prevTimer;
 
   var applyFilters = function () {
-    var toFilter = [];
-    for (var i = 0; i < window.pinsArray.length; i++) {
-      window.pinsArray[i].classList.add('hidden');
-      var applyFilterPrice = function () {
-        if (window.data[i].offer.price < 10000 && filters.price === 'low') {
-          return true;
-        } else if (window.data[i].offer.price >= 50000 && filters.price === 'high') {
-          return true;
-        } else if (window.data[i].offer.price >= 10000 &&
-                   window.data[i].offer.price < 50000 &&
-                   filters.price === 'middle') {
-          return true;
-        } else if (filters.price === 'any') {
-          return true;
-        } else {
-          return false;
-        }
-      };
-      var findFeatures = function (ind) {
-        if (window.data[i].offer.features[ind] === filters.features[ind]) {
-          return true;
-        } else if (filters.features[ind] === false) {
-          return true;
-        } else {
-          return false;
-        }
-      };
-
-      toFilter[0] = (window.data[i].offer.type === filters.housingType || filters.housingType === 'any');
-      toFilter[1] = applyFilterPrice();
-      toFilter[2] = (window.data[i].offer.rooms === +filters.rooms || filters.rooms === 'any');
-      toFilter[3] = (window.data[i].offer.guests === +filters.guests || filters.guests === 'any');
-      toFilter[4] = findFeatures(0);
-      toFilter[5] = findFeatures(1);
-      toFilter[6] = findFeatures(2);
-      toFilter[7] = findFeatures(3);
-      toFilter[8] = findFeatures(4);
-      toFilter[9] = findFeatures(5);
-
-      var usePinsFilters = function () {
-        for (var j = 0; j < toFilter.length; j++) {
-          if (toFilter[j] === false) {
-            window.pinsArray[i].classList.add('hidden');
-            break;
+    window.clearTimeout(prevTimer);
+    prevTimer = window.setTimeout(function () {
+      var toFilter = [];
+      for (var i = 0; i < window.pinsArray.length; i++) {
+        window.pinsArray[i].classList.add('hidden');
+        var applyFilterPrice = function () {
+          if (
+            (window.data[i].offer.price < 10000 && filters.price === 'low') ||
+            (window.data[i].offer.price >= 50000 && filters.price === 'high') ||
+            (window.data[i].offer.price >= 10000 && window.data[i].offer.price < 50000 && filters.price === 'middle') ||
+            (filters.price === 'any')
+          ) {
+            return true;
           } else {
-            window.pinsArray[i].classList.remove('hidden');
+            return false;
+          }
+        };
+        var findFeatures = function (ind) {
+          if (
+            (window.data[i].offer.features[ind] === filters.features[ind]) ||
+            (filters.features[ind] === false)
+          ) {
+            return true;
+          } else {
+            return false;
+          }
+        };
+
+        toFilter[0] = (window.data[i].offer.type === filters.housingType || filters.housingType === 'any');
+        toFilter[1] = applyFilterPrice();
+        toFilter[2] = (window.data[i].offer.rooms === +filters.rooms || filters.rooms === 'any');
+        toFilter[3] = (window.data[i].offer.guests === +filters.guests || filters.guests === 'any');
+        toFilter[4] = findFeatures(0);
+        toFilter[5] = findFeatures(1);
+        toFilter[6] = findFeatures(2);
+        toFilter[7] = findFeatures(3);
+        toFilter[8] = findFeatures(4);
+        toFilter[9] = findFeatures(5);
+
+        var usePinsFilters = function () {
+          for (var j = 0; j < toFilter.length; j++) {
+            if (toFilter[j] === false) {
+              window.pinsArray[i].classList.add('hidden');
+              break;
+            } else {
+              window.pinsArray[i].classList.remove('hidden');
+            }
+          }
+        };
+        usePinsFilters();
+      }
+
+      var pinsCount = 0;
+      if (window.pinsArray.length > 1) {
+        for (var j = 0; j < window.pinsArray.length; j++) {
+          if (!window.pinsArray[j].classList.contains('hidden')) {
+            if (pinsCount >= 5) {
+              window.pinsArray[j].classList.add('hidden');
+            }
+            pinsCount++;
           }
         }
-      };
-      usePinsFilters();
-    }
+      }
+
+    }, 500);
   };
 
 
@@ -172,20 +185,21 @@
     guests: 'any',
     features: [false, false, false, false, false, false],
   };
+
   filterHousingType.addEventListener('change', function () {
-    filters.housingType = this.value;
+    filters.housingType = filterHousingType.value;
     applyFilters();
   });
   filterPrice.addEventListener('change', function () {
-    filters.price = this.value;
+    filters.price = filterPrice.value;
     applyFilters();
   });
   filterRooms.addEventListener('change', function () {
-    filters.rooms = this.value;
+    filters.rooms = filterRooms.value;
     applyFilters();
   });
   filterGuests.addEventListener('change', function () {
-    filters.guests = this.value;
+    filters.guests = filterGuests.value;
     applyFilters();
   });
 
@@ -200,48 +214,29 @@
     }
   };
 
-  var prevTimer;
 
   filterWifi.addEventListener('change', function () {
-    window.clearTimeout(prevTimer);
-    prevTimer = window.setTimeout(function () {
-      doFilterOnOff(filterWifi, 0);
-    }, 500);
+    doFilterOnOff(filterWifi, 0);
   });
 
   filterDishwasher.addEventListener('change', function () {
-    window.clearTimeout(prevTimer);
-    prevTimer = window.setTimeout(function () {
-      doFilterOnOff(filterDishwasher, 1);
-    }, 500);
+    doFilterOnOff(filterDishwasher, 1);
   });
 
   filterParking.addEventListener('change', function () {
-    window.clearTimeout(prevTimer);
-    prevTimer = window.setTimeout(function () {
-      doFilterOnOff(filterParking, 2);
-    }, 500);
+    doFilterOnOff(filterParking, 2);
   });
 
   filterWasher.addEventListener('change', function () {
-    window.clearTimeout(prevTimer);
-    prevTimer = window.setTimeout(function () {
-      doFilterOnOff(filterWasher, 3);
-    }, 500);
+    doFilterOnOff(filterWasher, 3);
   });
 
   filterElevator.addEventListener('change', function () {
-    window.clearTimeout(prevTimer);
-    prevTimer = window.setTimeout(function () {
-      doFilterOnOff(filterElevator, 4);
-    }, 500);
+    doFilterOnOff(filterElevator, 4);
   });
 
   filterConditioner.addEventListener('change', function () {
-    window.clearTimeout(prevTimer);
-    prevTimer = window.setTimeout(function () {
-      doFilterOnOff(filterConditioner, 5);
-    }, 500);
+    doFilterOnOff(filterConditioner, 5);
   });
 
 })();
